16be54335eb7390229f0cc5abb4ab3b7d8385bed
Vanya <zozyle94@mail.ru>
2014-11-11 20:32:06 +0200

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
  resources :users
+ resources :sessions, only: [:new, :create, :destroy]
 
   #get 'static_pages/...'
   # You can have the root of your site routed with "root"
@@ -7,6 +8,8 @@ Rails.application.routes.draw do
 
   root  'static_pages#home'
   match '/signup',  to: 'users#new',            via: 'get'
+  match '/signin',  to: 'sessions#new',         via: 'get'
+  match '/signout', to: 'sessions#destroy',     via: 'delete'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
